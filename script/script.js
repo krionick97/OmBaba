0a79c2756dd30c43a74e8a0f4b2326cb32ddbcc5
krionick97 <mistermers8@gmail.com>
2019-01-19 17:52:49 +0300

    Add class, remove class

--- script/script.js
+++ script/script.js
@@ -1,20 +1,33 @@
 $(document).ready(function () {
 
   // Модальное меню, переключение на список
-  $('.satsangs__menu-list').click(function (e) { 
-    $('.satsangs__menu-mount').toggleClass('satsangs__menu--item-active');
-    $('.satsangs__menu-list').toggleClass('satsangs__menu--item-active');
-    $('.modal-menu').toggleClass('menu-visible');
-    $('.satsangs__button-box-1').toggleClass('menu-visible');
-    $('.satsangs__button-box-2').toggleClass('menu-visible');    
+  $('.satsangs__menu-list').click(function (e) {
+
+    // Переключение иконок
+    $('.satsangs__menu-mount').removeClass('satsangs__menu--item-active');
+    $('.satsangs__menu-list').addClass('satsangs__menu--item-active');
+
+    // Вызов модального меню
+    $('.modal-menu').addClass('menu-visible');
+
+    // Переключение кнопок
+    $('.satsangs__button-box-1').removeClass('menu-visible');
+    $('.satsangs__button-box-2').addClass('menu-visible'); 
   });
+  
   // Модальное меню, переключение на гору
-  $('.satsangs__menu-mount').click(function (e) { 
-    $('.satsangs__menu-list').toggleClass('satsangs__menu--item-active');
-    $('.satsangs__menu-mount').toggleClass('satsangs__menu--item-active');
-    $('.modal-menu').toggleClass('menu-visible');
-    $('.satsangs__button-box-2').toggleClass('menu-visible');
-    $('.satsangs__button-box-1').toggleClass('menu-visible');    
+  $('.satsangs__menu-mount').click(function (e) {
+
+    // Переключение иконок
+    $('.satsangs__menu-list').removeClass('satsangs__menu--item-active');
+    $('.satsangs__menu-mount').addClass('satsangs__menu--item-active');
+  
+    // Скрытие модального меню
+    $('.modal-menu').removeClass('menu-visible');
+  
+    // Переключение кнопок
+    $('.satsangs__button-box-2').removeClass('menu-visible');
+    $('.satsangs__button-box-1').addClass('menu-visible');
   });
   //////////////////////////////////////////////////////////////////////////////////////
 
